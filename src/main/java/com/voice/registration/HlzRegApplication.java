29cd07cdfa71603567dc8f59f2178ca7014f4ca2
saurkumar <saurav.kumar2@niceactimize.com>
2023-03-04 17:31:41 +0530

    initial commit with test controller

---
+++ src/main/java/com/voice/registration/HlzRegApplication.java
@@ -0,0 +1,17 @@
+package com.voice.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HlzRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlzRegApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/voice/registration/HlzRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voice.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlzRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
