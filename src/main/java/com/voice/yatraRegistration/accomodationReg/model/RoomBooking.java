03bef24379ce61adca1ecbb92d43afe247f4eb76
saurav <saurav109677@gmail.com>
2023-06-04 21:19:02 +0530

    added roomBooking

---
+++ src/main/java/com/voice/yatraRegistration/accomodationReg/model/RoomBooking.java
@@ -0,0 +1,46 @@
+package com.voice.yatraRegistration.accomodationReg.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SecondaryTable;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.voice.dbRegistration.model.DevoteeInfo;
+import com.voice.yatraRegistration.memberReg.model.Member;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class RoomBooking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<RoomSet> roomSet;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private AccTransaction accTransaction;
+    
+}
+
