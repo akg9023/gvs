dd478bb435fe222e7202253f3492ddbe4f5a8e7b
Saurav109677 <34793927+Saurav109677@users.noreply.github.com>
2023-07-21 13:22:30 +0530

    CORS in participant

--- src/main/java/com/voice/attendance/restController/JapaParticipanatsRestController.java
+++ src/main/java/com/voice/attendance/restController/JapaParticipanatsRestController.java
@@ -4,16 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.voice.attendance.dao.JapaParticipantsDao;
 import com.voice.attendance.models.JapaParticipants;
 
 @RestController
 @RequestMapping("/v1/participants/japa")
+@CrossOrigin(origins = "*")
 public class JapaParticipanatsRestController {
 
     @Autowired
