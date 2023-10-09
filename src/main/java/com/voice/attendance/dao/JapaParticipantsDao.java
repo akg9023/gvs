da1d718ce8575a4caf702fdc91623b6d30699b38
gaurav <saurav109677@gmail.com>
2023-10-09 14:31:22 +0530

    filter the participatnts Id according to faciliatator

--- src/main/java/com/voice/attendance/dao/JapaParticipantsDao.java
+++ src/main/java/com/voice/attendance/dao/JapaParticipantsDao.java
@@ -1,10 +1,16 @@
 package com.voice.attendance.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.voice.attendance.models.JapaParticipants;
 
 
 public interface JapaParticipantsDao extends JpaRepository<JapaParticipants,Long> {
     public JapaParticipants findOneByEmail(String email);
+
+    @Query(value = "SELECT m.id FROM JapaParticipants m where m.facId = ?1")
+    public List<Long> findAllFacilitesId(Long facId);
 }
