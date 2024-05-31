3e389e0fb464d1594267e7768964782af3e4bc58
Ved Prakash <ved.diav@gmail.com>
2024-05-31 14:51:47 +0530

    repositories added

---
+++ src/main/java/com/voice/auth/dao/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.voice.auth.dao;
+
+import com.voice.auth.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
