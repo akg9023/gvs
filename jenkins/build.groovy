a5d847001c2cb12a59286e5090a95701c7fea3ce
saurkumar <saurav.kumar2@niceactimize.com>
2025-07-19 20:57:18 +0530

    pass env

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -33,18 +33,18 @@ pipeline {
 //                     Find the generated JAR file
                     def jarFile = sh(script: "ls build/libs/GVS-0.0.1-SNAPSHOT.jar", returnStdout: true).trim()
                     echo "Running JAR: ${jarFile}"
-                    sh 'source /var/lib/jenkins/.bash_profile > /dev/null 2>&1' // Suppress output
+//                    sh 'source /var/lib/jenkins/app.env > /dev/null 2>&1' // Suppress output
 
                     // find the application and
-                    def pid = sh(script: "lsof -i :8443 | awk 'NR==2 {print \$2}'", returnStdout: true).trim()
-
-                    if (pid) {
-                        echo "Stopping process on port 8443 with PID: ${pid}"
-//                        sh "kill -9 ${pid}"
-                    } else {
-                        echo "No process running on port 8443"
-                    }
-                    sh "source /var/lib/jenkins/.bash_profile > /dev/null 2>&1 && export \$(cat /var/lib/jenkins/.bash_profile | xargs) && java -jar ${jarFile}"
+//                    def pid = sh(script: "lsof -i :8443 | awk 'NR==2 {print \$2}'", returnStdout: true).trim()
+//
+//                    if (pid) {
+//                        echo "Stopping process on port 8443 with PID: ${pid}"
+////                        sh "kill -9 ${pid}"
+//                    } else {
+//                        echo "No process running on port 8443"
+//                    }
+                    sh "source /var/lib/jenkins/app.env > /dev/null 2>&1 && java -jar ${jarFile}"
                 }
             }
         }
