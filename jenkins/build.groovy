c99918c943540f569722ea4345b637c76700ecba
saurkumar <saurav.kumar2@niceactimize.com>
2025-07-22 14:57:00 +0530

    setup

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -27,42 +27,41 @@ pipeline {
                         sudo -u ec2-user bash -c '
                         cd /home/ec2-user/gvs-server && \
                         sudo touch app.log && chmod 666 app.log && \
-                        nohup java -jar GVS-0.0.1-SNAPSHOT.jar > app.log 2>&1 & disown'
+                        nohup java -jar GVS-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &'
                     """
-                    sleep(time: 120, unit: 'SECONDS') // Allow some time for the process to start
-                    echo "Application started successfully in the background. Monitoring logs..."
+                    echo "Application started successfully in the background."
 
-                    def success = false
-                    def errorLine = null
-                    try {
-                        timeout(time: 120, unit: 'SECONDS') {
-                            waitUntil {
-                                def logContent = readFile('app.log')
-                                echo logContent
-                                if (logContent.contains("Started HlzRegApplication")) {
-                                    success = true
-                                    return true
-                                }
-                                def errorMatch = logContent =~ /(?i)Failed/
-                                if (errorMatch) {
-                                    errorLine = errorMatch[0]
-                                    return true
-                                }
-                                return false
-                            }
-                        }
-                    } catch (Exception e) {
-                        error "Timeout reached while monitoring logs."
-                    }
-
-                    if (success) {
-                        sleep(time: 120, unit: 'SECONDS')
-                        echo "Application started successfully."
-                    } else if (errorLine) {
-                        error "Application failed with error: ${errorLine}"
-                    } else {
-                        error "Application did not start successfully."
-                    }
+//                    def success = false
+//                    def errorLine = null
+//                    try {
+//                        timeout(time: 120, unit: 'SECONDS') {
+//                            waitUntil {
+//                                def logContent = readFile('app.log')
+//                                echo logContent
+//                                if (logContent.contains("Started HlzRegApplication")) {
+//                                    success = true
+//                                    return true
+//                                }
+//                                def errorMatch = logContent =~ /(?i)Failed/
+//                                if (errorMatch) {
+//                                    errorLine = errorMatch[0]
+//                                    return true
+//                                }
+//                                return false
+//                            }
+//                        }
+//                    } catch (Exception e) {
+//                        error "Timeout reached while monitoring logs."
+//                    }
+//
+//                    if (success) {
+//                        sleep(time: 120, unit: 'SECONDS')
+//                        echo "Application started successfully."
+//                    } else if (errorLine) {
+//                        error "Application failed with error: ${errorLine}"
+//                    } else {
+//                        error "Application did not start successfully."
+//                    }
                 }
             }
         }
